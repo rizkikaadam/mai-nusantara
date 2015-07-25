f4b329fbbc46c260b5ca9241efa0ca684b282856
Rizkika Adam Pratama <rizkikaadam@gmail.com>
2015-07-25 20:03:40 +0700

    dari laptop
    upload file pertama

---
+++ blog-awal.php
@@ -0,0 +1,56 @@
+<div class="two_third first">
+        <!-- ############################################## awal content di tengah ################################################## -->
+        <section class="main_slider">
+          <div class="rslides_container clear">
+            <ul class="rslides clear" id="rslides">
+              <li><img src="images/demo/slider/1.jpg" alt=""></li>
+              <li><img src="images/demo/slider/2.jpg" alt=""></li>
+              <li><img src="images/demo/slider/3.jpg" alt=""></li>
+            </ul>
+          </div>
+        </section>
+        <!-- #### -->
+        <article class="clear push50">
+          <div class="one_half first"><img src="images/demo/1200x400.gif" alt=""></div>
+          <div class="one_half">
+            <h2 class="nospace">Headline Text</h2>
+            <p>Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper.</p>
+            <footer><a href="#">Read More &raquo;</a></footer>
+          </div>
+        </article>
+		<article class="clear push50">
+          <div class="one_half first"><img src="images/demo/1200x400.gif" alt=""></div>
+          <div class="one_half">
+            <h2 class="nospace">Headline Text</h2>
+            <p>Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper.</p>
+            <footer><a href="#">Read More &raquo;</a></footer>
+          </div>
+        </article>
+        <article class="clear">
+          <div class="one_half first"><img src="images/demo/1200x400.gif" alt=""></div>
+          <div class="one_half">
+            <h2 class="nospace">Headline Text</h2>
+            <p>Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper.</p>
+            <footer><a href="#">Read More &raquo;</a></footer>
+          </div>
+        </article>
+		
+        <!-- #### -->
+        <div class="divider2"></div>
+        <div class="clear">
+      <!-- ################################################# awal list terbaru ############################################### -->
+		  <div class="toggle-wrapper"><a href="javascript:void(0)" class="toggle-title orange"><span>Terbaru</span></a>
+			<div class="toggle-content" style="display:block;">
+				<article class="clear push20">
+					<div class="imgl"><img src="images/demo/80x80.gif" alt=""></div>
+					<h2 class="font-medium nospace"><a href="#">Headline Text</a></h2>
+					<p class="nospace">Integer imperdiet vestibulum leo ut tincidunt in sagittis.</p>
+				</article>
+			</div>
+		  </div>
+      <!-- ######################################################### akhir list terbaru ####################################### -->
+		</div>
+        <!-- #### -->
+        <div class="clear"></div>
+        <!-- ############################################akhir content di tengah#################################################### -->
+</div>
---
+++ gallery-awal.php
@@ -0,0 +1,46 @@
+<div class="two_third first">
+    <div id="gallery">
+          <section>
+            <figure>
+              <h2>Galeri Kumpulan Kegiatan</h2>
+              <ul class="clear">
+                <li class="one_fifth first"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth first"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth first"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+                <li class="one_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+              </ul>
+              <figcaption>
+                <p>This is a W3C standards compliant free responsive HTML5 website template from <a href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a>. For full terms of use of this template please read our <a href="http://www.os-templates.com/template-terms">Website Template Licence</a>.</p>
+                <p>You can use and modify the template for both personal and commercial use. You must keep all copyright information and credit links in the template and associated files. For more responsive HTML5 templates visit <a href="http://www.os-templates.com/">Free Website Templates</a>.</p>
+              </figcaption>
+            </figure>
+          </section>
+          <!-- ####################################################################################################### -->
+          <nav class="pagination">
+            <ul>
+              <li class="prev"><a href="#">&laquo; Previous</a></li>
+              <li><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li class="splitter"><strong>&hellip;</strong></li>
+              <li><a href="#">6</a></li>
+              <li class="current"><strong>7</strong></li>
+              <li><a href="#">8</a></li>
+              <li class="splitter"><strong>&hellip;</strong></li>
+              <li><a href="#">14</a></li>
+              <li><a href="#">15</a></li>
+              <li class="next"><a href="#">Next &raquo;</a></li>
+            </ul>
+          </nav>
+        </div>
+</div>
